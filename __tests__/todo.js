6f04bde232effc3e99d3bb45b77f5955180cf1f8
yahshaswitha <yahshaswitha.sirineni@gmail.com>
2022-11-16 07:39:05 +0530

    tests added

---
+++ __tests__/todo.js
@@ -0,0 +1,63 @@
+/* eslint-disable no-undef */
+
+const todoList = require("../todo");
+const { all, add, markAsComplete, overdue, dueToday, dueLater } = todoList();
+
+describe("Todo List Test Suite", () => {
+  beforeAll(() => {
+    add({
+      title: "Test",
+      dueDate: new Date().toISOString().split("T")[0],
+      completed: false,
+    });
+  });
+
+  test("Add todo element", () => {
+    const todoLength = all.length;
+    add({
+      title: "Test",
+      dueDate: new Date().toISOString().split("T")[0],
+      completed: false,
+    });
+    expect(all.length).toBe(todoLength + 1);
+  });
+
+  test("Mark todo as complete", () => {
+    markAsComplete(0);
+    expect(all[0].completed).toBe(true);
+  });
+
+  test("Over due", () => {
+    overdueLength = overdue().length;
+    add({
+      title: "Test over due",
+      dueDate: new Date(new Date().setDate(new Date().getDate() - 1))
+        .toISOString()
+        .split("T")[0],
+      completed: false,
+    });
+    expect(overdue().length).toBe(overdueLength + 1);
+  });
+
+  test("Due today", () => {
+    dueTodayLength = dueToday().length;
+    add({
+      title: "Test due today",
+      dueDate: new Date().toISOString().split("T")[0],
+      completed: false,
+    });
+    expect(dueToday().length).toBe(dueTodayLength + 1);
+  });
+
+  test("Due later", () => {
+    dueLaterLength = dueLater().length;
+    add({
+      title: "Test due later",
+      dueDate: new Date(new Date().setDate(new Date().getDate() + 1))
+        .toISOString()
+        .split("T")[0],
+      completed: false,
+    });
+    expect(dueLater().length).toBe(dueLaterLength + 1);
+  });
+});
